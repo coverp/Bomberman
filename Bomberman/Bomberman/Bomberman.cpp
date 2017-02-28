5535f3d3182c5170645b30e23ad23b700b1bee9a
alexmwebber <17AMW03@stu.cpsk12.org>
2017-02-28 14:43:12 -0600

    SFML Working
    Got SFML working (hopefully)

--- Bomberman/Bomberman/Bomberman.cpp
+++ Bomberman/Bomberman/Bomberman.cpp
@@ -1,11 +1,25 @@
-// Bomberman.cpp : Defines the entry point for the console application.
-//
+#include <SFML/Graphics.hpp>
+//#include "stdafx.h"
 
-#include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
-
